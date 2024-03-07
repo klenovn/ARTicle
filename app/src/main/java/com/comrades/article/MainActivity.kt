baa372974adc88e56c644a98099bb1c2b7a40b67
Nikita Klenov <klenovn2002@gmail.com>
2024-03-07 11:39:34 +0300

    Created dev branch with empty activity

--- app/src/main/java/com/comrades/article/MainActivity.kt
+++ app/src/main/java/com/comrades/article/MainActivity.kt
@@ -1,4 +1,11 @@
 package com.comrades.article
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
