92c4c02bc0fffe8db548d368e031eefb8f8c8857
WhiskeyMoonlight <dmitriykaskader@gmail.com>
2024-03-11 21:43:28 +0300

    WIP: cleaned imports of ArcicleAdapter and MainFragment

--- app/src/main/java/com/comrades/article/ArticleAdapter.kt
+++ app/src/main/java/com/comrades/article/ArticleAdapter.kt
@@ -1,7 +1,5 @@
 package com.comrades.article
 
-import android.os.Bundle
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
